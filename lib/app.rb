7482761f11cc60c0a33d2c9feb5c9dafcca9d007
Otis <otisvickersgraver@gmail.com>
2020-10-15 15:13:56 +0100

    view player hitpoints, added test helper to keep feature tests DRY

--- lib/app.rb
+++ lib/app.rb
@@ -1,15 +1,28 @@
 require "sinatra/base"
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get "/" do
     erb(:index)
   end
 
   post "/names" do
-    @player1 = params["player1"]
-    @player2 = params["player2"]
+    session[:player1] = params["player1"]
+    session[:player2] = params["player2"]
+    redirect '/play'
+  end
+
+  get "/play" do
+    @player1_name = session[:player1]
+    @player2_name = session[:player2]
+    session[:player1_hp] = 100
+    session[:player2_hp] = 100
+    @player1_hp = session[:player1_hp]
+    @player2_hp = session[:player2_hp]
     erb(:play)
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,15 @@
 feature "Adding and displaying names" do
   scenario "Fill in two names in a form, submit the form and see the names on the screen" do
-    visit("/")
-    fill_in "player1", with: "Dan"
-    fill_in "player2", with: "Otis"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Dan vs. Otis")
+  end
+end
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+feature "View hitpoints" do
+  scenario "A player wants to view another players' hitpoints to see how close they are to winning" do
+    sign_in_and_play
+    expect(page).to have_content("Dan: 100 Hitpoints")
   end
 end
